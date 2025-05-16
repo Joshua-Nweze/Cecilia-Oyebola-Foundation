fe21fc3cf22a2fa39ca7c02f47d4bed09db02c55
Joshua-Nweze <joshuanweze270@gmail.com>
2025-05-16 07:03:09 +0100

    ui: update footer and nav components with new logo; add testimony carousel

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,13 +3,9 @@
     <footer class="bg-white dark:bg-gray-900">
         <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
             <div class="grid lg:grid-cols-2">
-            <div class="mb-6 md:mb-0 text-white">
-                logo
-                <!-- <a href="https://flowbite.com/" class="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 me-3" alt="FlowBite Logo" />
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-                </a> -->
-            </div>
+            <NuxtLink to="/" class="mb-6 md:mb-0 text-white">
+                <img src="/images/cecy-foundation-logo.png" class="h-40" alt="">
+            </NuxtLink>
             <div class="grid grid-cols-2 gap-8">
                 <div>
                     <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Links</h2>
--- components/Nav.vue
+++ components/Nav.vue
@@ -1,11 +1,8 @@
 <template>
     <header class="text-gray-400 bg-gray-900 body-font lg:px-30 fixed z-[999] top-0 left-0 right-0">
-        <div class="grid grid-cols-2 mx-auto  py-5  items-center">
+        <div class="grid grid-cols-2 mx-auto py-1 items-center">
             <NuxtLink to="/" class="w-fit">
-            <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-            </svg> -->
-                <span class="ml-3 text-xl">LOGO</span>
+                <img src="/images/cecy-foundation-logo.png" class="h-20" alt="">
             </NuxtLink>
             <nav class="flex flex-wrap items-center text-base justify-end">
                 <NuxtLink to="/" class="mr-5 hover:text-white">Home</NuxtLink>
